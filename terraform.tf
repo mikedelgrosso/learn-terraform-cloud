aba15909b41b7d7e28a2375ba566dde2157bd945
Mike DelGrosso <mikedelgrosso@gmail.com>
2023-05-24 22:22:51 -0500

    Nixed the cloud block for VCS integration demo

--- terraform.tf
+++ terraform.tf
@@ -1,17 +1,19 @@
 terraform {
+  # commenting the cloud block for VCS integration demo
+  # https://developer.hashicorp.com/terraform/tutorials/cloud-get-started/cloud-vcs-change
+  # cloud {
+  #   organization = "oddbull-development"
 
-  cloud {
-    organization = "organization-name"
-
-    workspaces {
-      name = "learn-terraform-cloud"
-    }
-  }
+  #   workspaces {
+  #     name = "learn-terraform-cloud"
+  #     description = "created s v 3.30.0"
+  #   }
+  # }
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.28.0"
+      version = "~> 3.30.0"
     }
   }
 
